d37d34a4be2c805e1288cafdde16ebde7bdb9a65
Zayna <156707902+zzainabb@users.noreply.github.com>
2025-12-01 23:27:15 -0600

    Add files via upload

---
+++ courseScheduleMedium.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto& p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+
+            for (int next : adj[node]) {
+                indegree[next]--;
+                if (indegree[next] == 0)
+                    q.push(next);
+            }
+        }
+        return count == numCourses;
+    }
+};
